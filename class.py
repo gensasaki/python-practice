2db2a0f8b6472a120cde05b531d86009e1de1b22
Gen Sasaki <sasakigen11@gmail.com>
2017-07-31 20:18:39 -0700

    Add how to restrict

--- class.py
+++ class.py
@@ -1,14 +1,19 @@
 # class
 # function and method
+# python can not restrict access right unlike private or protected
 
 class User:
+    # class variable
     count = 0
     def __init__(self, name):
+        # assign to class variable
         User.count += 1
-        self.name = name
+        # instance variable
+        # conventionally, restricted variable's name is __somthing
+        self.__name = name
     # instance method
     def say_hi(self):
-        print("hi {0}".format(self.name))
+        print("hi {0}".format(self.__name))
     # class method
     @classmethod
     def show_info(cls):
