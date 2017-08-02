0653cf62f2df68d7a0edf91e97eefa5dc8e70b56
Gen Sasaki <sasakigen11@gmail.com>
2017-08-01 20:19:10 -0700

    Add how to manage package

--- module.py
+++ module.py
@@ -1,12 +1,19 @@
 # module
 
 # import module2
-from module2 import AdminUser, User
+# from module2 import AdminUser, User
+
+# package
+
+# import package.module2 as mymodule
+from package.module2 import AdminUser
 
 # bob = module2.AdminUser("bob", 23)
+# bob = AdminUser("bob", 23)
+# bob = mymodule.AdminUser("bob", 23)
 bob = AdminUser("bob", 23)
 
-tom = User("tom")
+# tom = User("tom")
 
 print(bob.name)
 bob.say_hi()
